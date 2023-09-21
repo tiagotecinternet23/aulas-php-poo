af6a12d316158c824050d00db102217c136079c1
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-09-21 11:35:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo</title>
+    <title>Exemplo 1</title>
 </head>
 <body>
-    <h1>PHP com POO</h1>
+    <h1>PHP com POO - Exemplo 1</h1>
     <hr>
-    
+    <h2>Assuntos abordados:</h2>
+    <ul>
+        <li>Criação de classe</li>
+        <li>Importação do arquivo de classe</li>
+        <li>Criação de instâncias/objetos</li>
+    </ul>
+
+<?php
+// Importando a Classe
+require_once "src/Cliente.php";
+
+// Criando instâncias da Classe (objetos!)
+$clienteA = new Cliente;
+$clienteB = new Cliente;
+?>
+
+<pre><?=var_dump($clienteA, $clienteB)?></pre>
+
 </body>
 </html>
