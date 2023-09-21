b99ecadd53df40304740c76900177d802f560f42
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-09-21 11:57:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,26 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 1</title>
+    <title>Exemplo 2</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 1</h1>
+    <h1>PHP com POO - Exemplo 2</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Criação de classe</li>
-        <li>Importação do arquivo de classe</li>
-        <li>Criação de instâncias/objetos</li>
+        <li>Acesso direto às propriedades</li>
+        <li>Atribuição e leitura de dados</li>
+        <li>Chamada de método</li>
     </ul>
 
 <?php
-// Importando a Classe
 require_once "src/Cliente.php";
 
-// Criando instâncias da Classe (objetos!)
 $clienteA = new Cliente;
 $clienteB = new Cliente;
+
+// Acesso e atribuição
+$clienteA->nome = "Tiago";
+$clienteB->nome = "Bernardo";
+
+$clienteA->telefones = ["11-2135-0300", "11-96666-5555"];
+
+$clienteA->email = "tiago@gmail.com";
+$clienteA->senha = password_hash("123abc", PASSWORD_DEFAULT);
 ?>
+
+<hr>
+
+<h2>Dados dos objetos (acesso e leitura)</h2>
+<h3> <?= $clienteA->nome ?> </h3>
+<h3> <?= $clienteB->nome ?> </h3>
+
 
 <pre><?=var_dump($clienteA, $clienteB)?></pre>
 
