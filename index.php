11fa507aeac8aa060c043844dddcc33223f3f2f6
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-09-22 09:47:48 -0300

    Encapsulamento

--- index.php
+++ index.php
@@ -3,28 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 3</title>
+    <title>Exemplo 4</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 3</h1>
+    <h1>PHP com POO - Exemplo 4</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Método construtor com atribuição obrigatória de 
-        valores aos parâmetros/propriedades do objeto</li>
-        <li>Uso do <code>$this</code> para acesso às propriedades
-        dentro do objeto</li>
+        <li>Encapsulamento</li>
+        <li>Modificadores de visibilidade 
+        (public, private e protected)</li>
     </ul>
 
 <?php
 require_once "src/Cliente.php";
-$clienteA = new Cliente("Fulano", "fulano@gmail.com");
-$clienteB = new Cliente("Ozzy", "ozzy@msn.com");
+$clienteA = new Cliente;
 
-//$clienteA->telefones = ["11-2135-0300"];
+/* O código abaixo dará erro pois
+a propriedade é privada! */
+//$clienteA->nome = "Tiago";
+
+/* Usando o setter para atribuir um valor 
+à propriedade privada "nome" */
+$clienteA->setNome("Fulano da Silva");
+
 ?>
 
-<pre> <?=var_dump($clienteA, $clienteB)?> </pre>
+<pre> <?=var_dump($clienteA)?> </pre>
 
 
 </body>
--- src/Cliente.php
+++ src/Cliente.php
@@ -1,23 +1,12 @@
 <?php
 class Cliente {
 
-    public string $nome;
-    public string $email; 
-    public string $senha;
-    public array $telefones;
-
-    /* Método construtor
-    Permite configurar a forma de inicialiazação
-    do objeto, exigindo a atribução de dados
-    no momento de criar o objeto/instância.
-    O construtor é sempre chamado de forma
-    automática assim que o objeto é criado. */
-    public function __construct(string $nome, string $email) {
-        /* Atribuindo os valores dos parâmetros
-        às propriedades. Usamos o $this para acessar
-        as propriedades desta Classe. */
+    private string $nome;
+    private string $email; 
+    private string $senha;
+   
+    /* Métodos getters e setters */
+    public function setNome(string $nome):void {
         $this->nome = $nome;
-        $this->email = $email;
     }
-
 }
