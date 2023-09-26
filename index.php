b0abbf7d55bf7850f49ba30527e38cb96e649602
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-09-26 11:39:43 -0300

    Classe final

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     <ul>
         <li>Herança (Super Classe e Sub Classe)</li>
         <li>Métodos/propriedades protegidos (acessíveis apenas pela classe em que foram definidos e pelas subclasses)</li>
-        <li>Classe Abstrata</li>
-        <li>Classe Final</li>
+        <li>Classe Abstrata: não permite instâncias/objetos, serve apenas de modelo para subclasses.</li>
+        <li>Classe Final: não permite herança, ou seja, não aceita a criação de subclasses.</li>
     </ul>
 
 <?php
@@ -39,6 +39,17 @@ $clientePJ->setNomeFantasia("Bla Bla Informática");
 <pre> <?=var_dump($clientePF, $clientePJ)?> </pre>
 
 <hr>
+
+<?php
+require_once "src/MEI.php";
+$clienteMei = new MEI;
+$clienteMei->setNome("Marina Tanaka");
+$clienteMei->setAnoFundacao(2023);
+$clienteMei->setNomeFantasia("Tanaka Empreendimentos");
+$clienteMei->setAreaDeAtuacao("Ervas medicinais");
+?>
+<pre><?=var_dump($clienteMei)?></pre>
+
 
 <?php
 require_once "src/Cliente.php";
