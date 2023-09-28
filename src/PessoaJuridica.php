18c47f50171e201d675b0971ba70db50f87188d3
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-09-28 11:08:49 -0300

    Aplicação do exibirDados com sobreposição

--- src/PessoaJuridica.php
+++ src/PessoaJuridica.php
@@ -5,11 +5,19 @@ class PessoaJuridica extends Cliente {
     private string $cnpj;
     private string $nomeFantasia;
 
-    public function __construct()
-    {
+    public function __construct() {
         /* Acessando o método setter PROTEGIDO (existente em Cliente) */
         $this->setSituacao("em análise");
     }
+
+    public function exibirDados():void {
+        echo "<h3>Método exibirDados - Classe PessoaJuridica</h3>";
+        echo "<h4>".$this->getNome()."</h4>";
+        echo "<p> Situação: ".$this->getSituacao()."</p>";
+        echo "<p> Nome Fantasia: ".$this->nomeFantasia."</p>";
+    }
+
+
 
     public function getAnoFundacao(): int
     {
