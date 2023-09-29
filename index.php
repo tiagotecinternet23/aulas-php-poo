930e26d816ef1c579c58eae5ac5c1a8e24a2b12f
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-09-29 08:38:17 -0300

    Classes de Pagamento

--- index.php
+++ index.php
@@ -3,45 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 8</title>
+    <title>Exemplo 9</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 8</h1>
+    <h1>PHP com POO - Exemplo 9</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Propriedades e métodos estáticos</li>
-        <li>Acesso direto sem necessidade de objetos/instâncias</li>
-        <li>Uso do <code>self</code> para acesso (dentro da classe) aos recursos estáticos</li>
+        <li>Namespaces: agrupamento e organização de recursos (classes, funções, constantes)</li>
+        <li>Prevenção de conflitos entre classes de mesmo nome</li>
+        <li>Configurar e usar <code>namespaces</code> e <code>alias</code></li>
     </ul>
 
-<?php
-require_once "src/PessoaFisica.php";
-$cliente1 = new PessoaFisica;
-$cliente1->setNome("Astrogildo");
-$cliente1->setIdade(75);
 
-$cliente2 = new PessoaFisica;
-$cliente2->setNome("Enzo");
-$cliente2->setIdade(20);
-
-/* Usando os recursos estáticos */
-require_once "src/Utilitarios.php";
-Utilitarios::obterData();
-?>
-
-<h2>Atendimentos do dia: 
-<?=Utilitarios::$dataAtual?> </h2>
-
-<h3>Cliente: <?=$cliente1->getNome()?></h3>
-<p>Tipo de atendimento: 
-    <?=Utilitarios::definirAtendimento( $cliente1->getIdade() )?>
-</p>
-
-<h3>Cliente: <?=$cliente2->getNome()?></h3>
-<p>Tipo de atendimento: 
-    <?=Utilitarios::definirAtendimento( $cliente2->getIdade() )?>
-</p>
 
 </body>
 </html>
